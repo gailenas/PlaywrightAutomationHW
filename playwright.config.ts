7d4ec5fdfbf262b69735a6a39fe5caea74d726f7
Gailius Besusparis <gailenas@yahoo.com>
2026-02-09 10:01:15 +0200

    Enable traces on all retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
     baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-all-retries',
     screenshot: 'only-on-failure',
   },
 
